7f2b74a05a5dfb4af60ab0e2e27b58cf1fd4c971
BYU-ronel <esp24008@byui.edu>
2024-10-03 16:17:54 +0800

    modified

---
+++ scripts/place.js
@@ -0,0 +1,23 @@
+const data = {
+    "title": "Philippines",
+    "data": {
+      "Area": "587,041 sq km",
+      "Population": "27,691,018",
+      "Capital": "Manila",
+      "Languages": "English, Tagalog",
+      "Currency": "Philippine Peso",
+      "Time Zone": "UTC+8",
+      "Calling Code": "+639",
+      "Internet TLD": ".gb"
+    },
+    "weather": {
+      "Temperature": "30 °C",
+      "Conditions": "Sunny",
+      "Wind": "5 km/h",
+      "Wind Chill": "9.8 °C"
+    },
+    "credits": {
+      "Author": "Ronel Espaldon",
+      "Last Modification": "02/08/2024 22:47:47"
+    }
+  };
