de32437f8f091f367e8d24ff0d9e7532e76350f6
BYU-ronel <esp24008@byui.edu>
2024-10-21 14:18:59 +0800

    modified

--- scripts/products.js
+++ scripts/products.js
@@ -1,18 +1,10 @@
-// Get the current year
-const currentYear = new Date().getFullYear();
+// Get the current year and last modified date
+const yearElement = document.getElementById('year');
+const lastModifiedElement = document.getElementById('last-modified');
 
-// Get the last modified date
-const lastModified = document.lastModified;
-
-// Get the footer paragraphs
-const footerParagraphs = document.querySelectorAll('footer p');
-
-// Set the copyright year in the first paragraph
-footerParagraphs[0].textContent = `Copyright ${currentYear}`;
-
-// Set the last modified date in the second paragraph
-footerParagraphs[1].id = 'lastModified';
-footerParagraphs[1].textContent = `Last modified: ${lastModified}`;
+const date = new Date();
+yearElement.textContent = date.getFullYear();
+lastModifiedElement.textContent = `Last Modified: ${date.toLocaleDateString()}`;
 
 const products = [
     {
